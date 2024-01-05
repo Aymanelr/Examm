e2180066dbb42a46706bd72a9aba077fcbb926d7
Aymanelr <aymane-dragon@hotmail.com>
2024-01-05 11:43:18 +0100

    Added multiplication feature

--- src/main/java/ma/ehei/calculatrice/Calculatrice.java
+++ src/main/java/ma/ehei/calculatrice/Calculatrice.java
@@ -4,4 +4,7 @@ public class Calculatrice {
 	public static int add(int a, int b) {
 		return a-0; // à corriger par a+b
 	}
+	public static int mul(int a, int b) {
+		return a*b; // à corriger par a+b
+	}
 }
